ac8f4831715f72bc268a7033479e1179b4e0bc45
Rigney <krigney@proforma.com>
2018-03-13 15:32:19 -0400

    added working code

--- public/main.js
+++ public/main.js
@@ -1,5 +1,78 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+// As a user I should be able to see three buttons for each player
+// as a user, I should be able to pick an option for each player
+
+let playerOneSelection = "no selection";
+
+const playerOneSelectsRock = () => {
+  document.querySelector('#playerOneRock').style.backgroundColor = "blue";
+  console.log("rock")
+  playerOneSelection = "rock";
+  gameOver()
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const playerOneSelectsPaper = () => {
+  document.querySelector('#playerOnePaper').style.backgroundColor = "blue";
+  playerOneSelection = "paper";
+  gameOver()
+}
+
+const playerOneSelectsScissors = () => {
+  document.querySelector('#playerOneScissors').style.backgroundColor = "blue";
+  playerOneSelection = "scissors";
+  gameOver()
+}
+
+
+let playerTwoSelection = "no selection";
+
+const playerTwoSelectsRock = () => {
+  document.querySelector('#playerTwoRock').style.backgroundColor = "blue";
+  playerTwoSelection = "rock";
+  gameOver()
+}
+
+const playerTwoSelectsPaper = () => {
+  document.querySelector('#playerTwoPaper').style.backgroundColor = "blue";
+  playerTwoSelection = "paper";
+  gameOver()
+}
+
+const playerTwoSelectsScissors = () => {
+  document.querySelector('#playerTwoScissors').style.backgroundColor = "blue";
+  playerTwoSelection = "scissors";
+  gameOver()
+}
+
+// As a user,after each player makes their selection, I should be told who won and the game should be over
+
+
+const gameOver = () => {
+  if ((playerOneSelection === "rock" || playerOneSelection === "paper" || playerOneSelection === "scissors") && (playerTwoSelection === "rock" || playerTwoSelection === "paper" || playerTwoSelection === "scissors")) {
+    console.log("Players have made selections")
+    winnerAlert()
+    }
+  }
+
+  const winnerAlert = () => {
+    //when one button from each player is selcted
+    // when gameOver occurs, winnerAlert should start.
+    if (playerOneSelection === "rock" && playerTwoSelection === "scissors") {
+      console.log("Player One Wins!")
+      alert("Player One Wins!")
+    } else if (playerOneSelection === "paper" && playerTwoSelection === "rock") {
+      console.log("Player One Wins!")
+      alert("Player One Wins!")
+    } else if (playerOneSelection === "scissors" && playerTwoSelection === "paper") {
+      console.log("Player One Wins!")
+      alert("Player One Wins!")
+    } else if (playerTwoSelection === "rock" && playerOneSelection === "scissors") {
+      console.log("Player Two Wins!")
+      alert("Player Two Wins!")
+    } else if (playerTwoSelection === "paper" && playerOneSelection === "rock") {
+      alert("Player Two Wins!")
+    } else if (playerTwoSelection === "scissors" && playerOneSelection === "paper") {
+      alert("Player Two Wins!")
+    } else {
+      alert("It's a tie!")
+    }
+  }
